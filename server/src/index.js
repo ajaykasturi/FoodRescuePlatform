8076fe9394eedbdbbcda63031ec1347aeb12b66d
AjayKasturi <ajayreigns0@gmail.com>
2025-11-09 20:11:17 -0500

    server: set-up basic express server

---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
